7b8b07619fc2b9671be29c8535fa8013753ed8f1
karan-m <75066366+karan-mudaliar@users.noreply.github.com>
2023-08-25 10:26:57 -0400

    Time: 41 ms (67.1%), Space: 16.3 MB (36.93%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,18 +1,15 @@
-class Solution(object):
-    def rob(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-
-        if len(nums) <=2:
-            return max(nums)
-        
-        DP = nums[:]
-
-        DP[1] = max(DP[0],DP[1])
-
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        DP = [0 for _ in range(len(nums))]
+        if len(nums) == 1:
+            return nums[0]
+        DP[0] = nums[0]
+        DP[1] = max(nums[0],nums[1])
+        if len(nums) == 2:
+            return DP[1]
         for i in range(2,len(nums)):
-            DP[i] = max(DP[i-1],DP[i-2]+nums[i])
-
+            DP[i] = max(DP[i-1],nums[i]+DP[i-2])
+        print(nums)
+        print(DP)
         return max(DP[-1],DP[-2])
+        
