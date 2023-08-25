669ae734c27a1f3537b43d566c8db32b0f4e9448
karan-m <75066366+karan-mudaliar@users.noreply.github.com>
2023-08-25 18:30:10 -0400

    Time: 907 ms (87.44%), Space: 16.6 MB (93.2%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,11 +1,25 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        arr = [float('inf')]*(amount+1)
+        arr = [float('inf')] * (amount+1)
         arr[0] = 0
-        
-        for coin in coins:
-            for amt in range(amount+1):
+        for amt in range(1,amount+1):
+            for coin in coins:
                 if amt >= coin:
-                    arr[amt] = min(arr[amt],1+arr[amt-coin])
-        return arr[amount] if arr[amount] != float('inf') else -1
+                    arr[amt] = min(arr[amt - coin] + 1, arr[amt])
+        
+        return arr[-1] if arr[-1] != float('inf') else -1
+        
+# class Solution:
+#     def coinChange(self, coins: List[int], amount: int) -> int:
+#         arr = [float('inf')]* (amount+1)
+#         arr[0] = 0
+
+#         for coin in coins:
+#             for amt in range(1,amount+1):
+#                 if amt >= coin:
+#                     arr[amt] = min(arr[amt],1 + arr[amt-coin])
+        
+#         return arr[-1] if arr[-1] != float('inf') else -1
+
+        
         
