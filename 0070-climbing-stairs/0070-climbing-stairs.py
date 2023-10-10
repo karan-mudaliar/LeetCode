e0335ac0635f6e8fa72f72c83420c889d2cd0334
karan-m <75066366+karan-mudaliar@users.noreply.github.com>
2023-10-09 23:09:16 -0400

    Time: 40 ms (42.02%), Space: 16.1 MB (80.37%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,14 +1,11 @@
 class Solution:
-    def climbStairs(self, n: int, i = 0,seen= {}) -> int:
-        
-        if n == 0:
-            return 1
+    def climbStairs(self, n: int,mem = None) -> int:
+        if mem is None:
+            mem = {0:1,1:1}
         if n < 0:
             return 0
-        if (n,i) in seen:
-            return seen[(n,i)]
+        if n in mem:
+            return mem[n]
+        mem[n] = self.climbStairs(n - 1,mem) + self.climbStairs(n - 2,mem)
+        return mem[n]
         
-        ways = self.climbStairs(n - 1) + self.climbStairs(n - 2)
-        seen[(n,i)] = ways
-        
-        return ways
